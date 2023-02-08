85a97f7fd6b7d25e2a5fb03f759a2bd0b369a9b2
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-08 08:47:14 -0500

    practiced react

---
+++ src/components/Message.jsx
@@ -0,0 +1,10 @@
+export default function Message() {
+    return (
+    <main>
+        <div>
+            <h1>You will always be my Valentine. Forever in my heart
+            </h1>
+        </div>
+     </main>
+    )
+}
